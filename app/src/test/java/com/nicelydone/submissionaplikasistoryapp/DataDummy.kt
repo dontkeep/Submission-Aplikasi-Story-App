e532c52234e4505bd8439e7fd0f16f160f1d2e71
dontkeep <muchammadromadona71@smk.belajar.id>
2024-08-15 12:10:51 +0700

    Final Submission DONE! Updated DONE FR THIS TIME

--- app/src/test/java/com/nicelydone/submissionaplikasistoryapp/DataDummy.kt
+++ app/src/test/java/com/nicelydone/submissionaplikasistoryapp/DataDummy.kt
@@ -1,12 +1,13 @@
 package com.nicelydone.submissionaplikasistoryapp
 
 import com.nicelydone.submissionaplikasistoryapp.model.connection.responses.ListStoryItem
+import com.nicelydone.submissionaplikasistoryapp.model.room.entity.StoryEntity
 
 object DataDummy {
-   fun generateDummyStoryResponse(): List<ListStoryItem> {
-      val items: MutableList<ListStoryItem> = arrayListOf()
+   fun generateDummyStoryResponse(): List<StoryEntity> {
+      val items: MutableList<StoryEntity> = arrayListOf()
       for (i in 0 until 100) {
-         val story = ListStoryItem(
+         val story = StoryEntity(
             id = i.toString(),
             createdAt = "createdAt + $i",
             name = "name $i",
